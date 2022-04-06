d30e0679b6521d95a93666b1b2419e61520bd10f
Gmol <arthur.gmol@gmail.com>
2022-04-06 10:11:58 +0100

    Add alert state

---
+++ states/AlertState.py
@@ -0,0 +1,28 @@
+import datetime
+import logging
+
+from states.Config import LightColor, LightEffect, OVERTIME, Activity, OVERTIME_ALERT
+from states.RestState import RestState
+from states.State import State
+
+
+class AlertState(State):
+
+    def __init__(self, context):
+        super().__init__(context)
+        self.name = self.__class__.__name__
+        self.logger = logging.getLogger(self.name)
+        self.timer = context.get_current_time()
+        self.logger.debug(f"* [{self.name}] created [${self.timer}]")
+        # self.context.light_on(LightEffect.SOLID_RED, {'color': LightColor.RED})
+
+    def evaluate(self, activity) -> None:
+        if activity == Activity.IDLE:
+            self.logger.info("----->  Idle activity move to rest")
+            self.context.light_off()
+            # Create Break state and switch
+            self.context.change_state(RestState(self.context, self))
+        else:
+            self.context.light_on(LightEffect.BLINKING)
+            self.logger.info(
+                "----->  Working alert! Timer[{}]".format(round(self.context.get_current_time() - self.timer)))
--- states/WorkState.py
+++ states/WorkState.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 
+from states.AlertState import AlertState
 from states.Config import LightColor, LightEffect, OVERTIME, Activity, OVERTIME_ALERT
 from states.RestState import RestState
 from states.State import State
@@ -11,9 +12,10 @@ class WorkState(State):
 
     def __init__(self, context):
         super().__init__(context)
-        self.logger = logging.getLogger("WorkState")
+        self.name = self.__class__.__name__
+        self.logger = logging.getLogger(self.name)
         self.timer = context.get_current_time()
-        self.logger.debug(f"* WorkStare created [${self.timer}]")
+        self.logger.debug(f"* [{self.name}] created [${self.timer}]")
         # self.context.light_on(LightEffect.SOLID_RED, {'color': LightColor.RED})
 
     def evaluate(self, activity) -> None:
@@ -23,15 +25,14 @@ class WorkState(State):
             self.context.light_off()
             # Create Break state and switch
             self.context.change_state(RestState(self.context, self))
+        elif self.context.get_current_time() - self.timer > OVERTIME_ALERT:
+            self.context.change_state(AlertState(self.context, self))
         elif self.context.get_current_time() - self.timer > OVERTIME:
             self.logger.info("----->  OVERTIME[{}m] turn the alarm on! timer[{}], timer+OVERTIME[{}], current time[{}]"
                              .format(round((self.context.get_current_time() - self.timer)/60), round(self.timer),
                                      round(self.timer + OVERTIME),
                                      round(self.context.get_current_time())))
-            if (self.context.get_current_time() - self.timer) > OVERTIME_ALERT:
-                self.context.light_on(LightEffect.BLINKING)
-            else:
-                self.context.light_on(LightEffect.SOLID_RED)
+            self.context.light_on(LightEffect.SOLID_RED)
         else:
             self.logger.info(
                 "----->  Working time! Timer[{}]".format(round(self.context.get_current_time() - self.timer)))
@@ -42,7 +43,7 @@ class WorkState(State):
         current_time = self.context.get_current_time()
         time_difference = current_time - self.timer
         if time_difference > self.UNACCEPTED_DIFFERENCE:
-            self.logger.warning(f"WorkState timer fixed from "
+            self.logger.warning(f"[{self.name}] timer fixed from "
                                 f"[{datetime.datetime.fromtimestamp(self.timer).strftime('%Y-%m-%d %H:%M:%S')}] "
                                 f"to [{datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')}]")
             self.timer = current_time
