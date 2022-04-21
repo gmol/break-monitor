7010960608abaa811e8c65e6f4970428733091bb
Gmol <arthur.gmol@gmail.com>
2022-04-21 17:58:12 +0100

    Add try-catch for mqtt

--- mqtt/MqttNotifier.py
+++ mqtt/MqttNotifier.py
@@ -14,10 +14,20 @@ class MqttNotifier:
         if MqttConfig.PASSWORD:
             payload = state.__class__.__name__
             self.logger.info(f"Publish state [{payload}] to MQTT topic [{MqttConfig.TOPIC}] "
-                             f"to broken [{MqttConfig.HOST}], username[{MqttConfig.USERNAME}],"
+                             f"to broker [{MqttConfig.HOST}], username[{MqttConfig.USERNAME}],"
                              f" password[{MqttConfig.PASSWORD}] ")
-            auth = {"username": MqttConfig.USERNAME, "password":  MqttConfig.PASSWORD}
+            auth = {"username": MqttConfig.USERNAME, "password": MqttConfig.PASSWORD}
 
-            publish.single(MqttConfig.TOPIC, payload, hostname=MqttConfig.HOST, auth=auth)
+            try:
+                publish.single(MqttConfig.TOPIC, payload, hostname=MqttConfig.HOST, auth=auth)
+            except Exception as ex:
+                template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                message = template.format(type(ex).__name__, ex.args)
+                self.logger.error(message)
+                self.logger.exception(ex)
+                self.logger.error(f"Publish state [{payload}] to MQTT topic [{MqttConfig.TOPIC}] "
+                                  f"to broker [{MqttConfig.HOST}], username[{MqttConfig.USERNAME}],"
+                                  f" password[{MqttConfig.PASSWORD}] failed!")
+                pass
         else:
             self.logger.info("MQTT password not set. Skip publishing to MQTT.")
