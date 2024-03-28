7b0222fbca2f5ec0af6317bdc1b1295e0e3a313b
Arthur Gmol <arthur.gmol@gmail.com>
2024-03-28 12:39:05 +0000

    Add new Detection Strategy

--- detector/DistanceThresholdCounterCoeffVar.py
+++ detector/DistanceThresholdCounterCoeffVar.py
@@ -55,6 +55,7 @@ class DistanceThresholdCounterCoefficientVar(DistanceThresholdCounter):
             return None
         else:
             last_n_samples = map(lambda s: s.distance, data[-n:])
+            self.logger.info("Last n samples: {}".format(last_n_samples))
             # last_n_samples = data[-n:]
             mean = np.mean(last_n_samples)
             std_dev = np.std(last_n_samples, ddof=1)  # Use ddof=1 to calculate unbiased estimate of standard deviation
